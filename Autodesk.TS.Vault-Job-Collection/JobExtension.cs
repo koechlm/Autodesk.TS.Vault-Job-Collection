cd1e7719a1369934d71073eb4919589c147a24a3
Markus Koechl <markus.koechl@autodesk.com>
2025-08-29 20:00:15 +0200

    Add project files.

---
+++ Autodesk.TS.Vault-Job-Collection/JobExtension.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Connectivity.Extensibility.Framework;
+using Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities;
+using Autodesk.Connectivity.JobProcessor.Extensibility;
+using Autodesk.Connectivity.WebServices;
+
+// *ComponentUpgradeEveryRelease-Client*
+[assembly: ApiVersion("19.0")]
+[assembly: ExtensionId("120bee0d-6c5b-4399-8a20-bb030f69ab4d")]
+
+
+namespace Autodesk.TS.Vault_Job_Collection
+{
+    public class JobExtension : IJobHandler
+    {
+        private static string JOB_TYPE = "MyCompany.JobName";
+
+        #region IJobHandler Implementation
+        public bool CanProcess(string jobType)
+        {
+            return jobType == JOB_TYPE;
+        }
+
+        public JobOutcome Execute(IJobProcessorServices context, IJob job)
+        {
+            try
+            {
+                MessageBox.Show("Hello World", "Job-Template-Messenger", MessageBoxButtons.OK);
+                return JobOutcome.Success;
+            }
+            catch (Exception ex)
+            {
+                context.Log(ex, "Job-Template Job failed: " + ex.ToString() + " ");
+                return JobOutcome.Failure;
+            }
+
+        }
+
+        public void OnJobProcessorShutdown(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorSleep(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorStartup(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorWake(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+        #endregion IJobHandler Implementation
+    }
+}
---
+++ adsk.ts.image.create.inventor/JobExtension.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Connectivity.Extensibility.Framework;
+using Autodesk.DataManagement.Client.Framework.Vault.Currency.Entities;
+using Autodesk.Connectivity.JobProcessor.Extensibility;
+using Autodesk.Connectivity.WebServices;
+
+// *ComponentUpgradeEveryRelease-Client*
+[assembly: ApiVersion("19.0")]
+[assembly: ExtensionId("a0574014-3e9a-4276-8dd8-2d54777a68d6")]
+
+
+namespace adsk.ts.image.create.inventor
+{
+    public class JobExtension : IJobHandler
+    {
+        private static string JOB_TYPE = "MyCompany.JobName";
+
+        #region IJobHandler Implementation
+        public bool CanProcess(string jobType)
+        {
+            return jobType == JOB_TYPE;
+        }
+
+        public JobOutcome Execute(IJobProcessorServices context, IJob job)
+        {
+            try
+            {
+                MessageBox.Show("Hello World", "Job-Template-Messenger", MessageBoxButtons.OK);
+                return JobOutcome.Success;
+            }
+            catch (Exception ex)
+            {
+                context.Log(ex, "Job-Template Job failed: " + ex.ToString() + " ");
+                return JobOutcome.Failure;
+            }
+
+        }
+
+        public void OnJobProcessorShutdown(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorSleep(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorStartup(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnJobProcessorWake(IJobProcessorServices context)
+        {
+            //throw new NotImplementedException();
+        }
+        #endregion IJobHandler Implementation
+    }
+}
