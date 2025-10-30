fc1151aa2b947278c6cebdd92e0777322de0c562
Markus Koechl <markus.koechl@autodesk.com>
2025-10-30 22:43:41 +0100

    updated pdf.create.slddrw version info

--- adsk.ts.pdf.create.slddrw/Properties/AssemblyInfo.cs
+++ adsk.ts.pdf.create.slddrw/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("31.0.84.0")]
-[assembly: AssemblyFileVersion("31.0.84.0")]
+[assembly: AssemblyVersion("31.0.84.1")]
+[assembly: AssemblyFileVersion("31.0.84.1")]
 #if NET6_0_OR_GREATER
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("windows7.0")]
 #endif
